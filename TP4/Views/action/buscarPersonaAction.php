cd9961a480c16f2ca5d49f7273995486e805b5eb
jehenriquez <44987583+jehenriquez@users.noreply.github.com>
2021-09-24 18:28:43 -0300

    Update buscarPersonaAction.php

--- TP4/Views/action/buscarPersonaAction.php
+++ TP4/Views/action/buscarPersonaAction.php
@@ -5,8 +5,8 @@ include_once "../../Controllers/ControllerPersona.php";
 
 $objPers = new ControllerPersona();
 $datos = data_submitted();
-#$resp = $datos['dni'];
-if (isset($datos['NroDni'])) {
+$persona = array();
+if (!empty($datos)) {
     $persona = $objPers->buscarPersona($datos);
     if (count($persona) == 1) {
         $objPer = $persona[0];
@@ -22,56 +22,62 @@ if (isset($datos['NroDni'])) {
 ?>
 
 <div class="container p-5 mt-5">
-    <div class="text-center">
-        <h4 class=""><i class=""></i> Datos Personales</h4>
+    <?php
+    if (!empty($persona)) {
+        echo "<div class='text-center'>
+        <h4 class=''><i class=''></i> Datos Personales</h4>
     </div>
     <div>
-    <form id="actualizar" name="actualizar" method="post" action="../actualizarDatosPersona.php">
-        <div class="row p-2">
-            <div class="form-group col-3">
-                <label for="NroDni">DNI</label>
-                <input id="NroDni" name="NroDni" value="<?php echo $dni ?>" type="text" class="form-control bg-light" readonly>
+    <form id='actualizar' name='actualizar' method='post' action='../actualizarDatosPersona.php'>
+        <div class='row p-2'>
+            <div class='form-group col-3'>
+                <label for='NroDni'>DNI</label>
+                <input id='NroDni' name='NroDni' value='{$dni}' type='text' class='form-control bg-light' readonly>
             </div>
         </div>
-        <div class="row p-2">
-            <div class="form-group col-6">
-                <label for="apellido">Apellido</label>
-                <input id="apellido" name="apellido" value="<?php echo $apellido ?>" type="text" class="form-control" required>
-                <div class="invalid-feedback">Ingrese el apellido</div>
+        <div class='row p-2'>
+            <div class='form-group col-6'>
+                <label for='apellido'>Apellido</label>
+                <input id='apellido' name='apellido' value='{$apellido}' type='text' class='form-control' required>
+                <div class='invalid-feedback'>Ingrese el apellido</div>
             </div>
-            <div class="form-group col-6">
-                <label for="nombre">Nombre</label>
-                <input id="nombre" name="nombre" value="<?php echo $nombre ?> " type="text" class="form-control" required>
-                <div class="invalid-feedback">Ingrese el nombre</div>
-            </div>
-        </div>
-        <div class="row p-2">
-            <div class="form-group col-6">
-                <label for="fechaNac">Fecha de nacimiento</label>
-                <input type="date" id="fechaNac" name="fechaNac" value="<?php echo $fechaNac ?>" type="text" class="form-control" required>
-                <div class="invalid-feedback">Ingrese la fecha de nacimiento</div>
-            </div>
-            <div class="form-group col-6">
-                <label for="telefono">Telefono</label>
-                <input id="telefono" name="telefono" value="<?php echo $telefono ?>" type="text" class="form-control" required>
-                <div class="invalid-feedback">Ingrese el telefono</div>
+            <div class='form-group col-6'>
+                <label for='nombre'>Nombre</label>
+                <input id='nombre' name='nombre' value='{$nombre}' type='text' class='form-control' required>
+                <div class='invalid-feedback'>Ingrese el nombre</div>
             </div>
         </div>
-        <div class="row p-2">
-            <div class="form-group col-6">
-                <label for="domicilio">Domicilio</label>
-                <input id="domicilio" name="domicilio" value="<?php echo $domicilio ?>" type="text" class="form-control" required>
-                <div class="invalid-feedback">Ingrese el domicilio</div>
+        <div class='row p-2'>
+            <div class='form-group col-6'>
+                <label for='fechaNac'>Fecha de nacimiento</label>
+                <input type='date' id='fechaNac' name='fechaNac' value='{$fechaNac}' type='text' class='form-control' required>
+                <div class='invalid-feedback'>Ingrese la fecha de nacimiento</div>
+            </div>
+            <div class='form-group col-6'>
+                <label for='telefono'>Telefono</label>
+                <input id='telefono' name='telefono' value='{$telefono}' type='text' class='form-control' required>
+                <div class='invalid-feedback'>Ingrese el telefono</div>
             </div>
         </div>
-        <div class="row p-1 justify-content-end">
-            <div class="form-group col-2">
-                <button name="submit" type="submit" class="btn btn-primary">Actualizar</button>
+        <div class='row p-2'>
+            <div class='form-group col-6'>
+                <label for='domicilio'>Domicilio</label>
+                <input id='domicilio' name='domicilio' value='{$domicilio}' type='text' class='form-control' required>
+                <div class='invalid-feedback'>Ingrese el domicilio</div>
             </div>
-            <button class="btn btn-primary bg-dark col-1" type="button" onclick="window.history.back()">Volver</button>
+        </div>
+        <div class='row p-1 justify-content-end'>
+            <div class='form-group col-2'>
+                <button name='submit' type='submit' class='btn btn-primary'>Actualizar</button>
+            </div>
+            <button class='btn btn-primary bg-dark col-1' type='button' onclick='window.history.back()'>Volver</button>
         </div>
     </form>
-    </div>
+    </div>";
+    } else {
+        echo "<div class='alert alert-info' role='alert'>¡No hay personas registrados con ese DNI!</div>";
+    }
+    ?>
 
     
 
