b02641e8a28b356e93ca20277cb2ea4270f59e45
jehenriquez <44987583+jehenriquez@users.noreply.github.com>
2021-09-24 18:27:20 -0300

    Update validaciones.js

--- TP4/Views/js/validaciones.js
+++ TP4/Views/js/validaciones.js
@@ -1,5 +1,22 @@
-function buscarPersona() {
-    alert("Llego");
+function validarDNI() {
+    exito = true;
+    var dni = $('#NroDni')[0];
+    //DNI
+    if (dni.value === "" || isNaN(parseInt(dni.value)) || dni.value < 0 || dni.value.length<7 || dni.value.length>8) { 
+        if (dni.classList.contains("is-valid")) {
+            dni.classList.remove("is-valid");
+        }
+        dni.classList.add("is-invalid");
+        exito = false;
+    } else {
+        if (dni.classList.contains("is-invalid")) {
+            dni.classList.remove("is-invalid");
+        }
+        dni.classList.add("is-valid");
+    }
+    return exito;
+};
+function validarPersona(){
+
     
-    
-}
+};
