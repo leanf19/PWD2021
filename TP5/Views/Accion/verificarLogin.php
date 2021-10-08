0c21cb1a071dbde076eae8dc984c5d46a61278ce
Matias <maty.infante@gmail.com>
2021-10-08 15:21:22 -0300

    minor fixes

--- TP5/Views/Accion/verificarLogin.php
+++ TP5/Views/Accion/verificarLogin.php
@@ -6,9 +6,9 @@ $sesion = new Session();
 $sesion->iniciar($datos['usnombre'], $datos['uspass']);
 
 if ($sesion->validar()) {
-    header('location:../paginaSegura.php');
+    header('location:../index/paginaSeguro.php');
 } else {
-    header('location:../login.php');
+    header('location:../index/login.php');
 }
 exit();
 ?>
